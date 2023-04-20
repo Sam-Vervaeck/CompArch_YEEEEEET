83d30f6d45fc759adec84643c42e91878621b0c5
Martin Van Wymeersch <53071347+Leon-MartinVW@users.noreply.github.com>
2023-04-20 11:03:57 +0200

    Add files via upload

--- CA_Exercises/Verilog/RTL/control_unit.v
+++ CA_Exercises/Verilog/RTL/control_unit.v
@@ -18,12 +18,15 @@ module control_unit(
    parameter integer ALU_R      = 7'b0110011;
    parameter integer ALU_I      = 7'b0010011;
    parameter integer BRANCH_EQ  = 7'b1100011;
-   parameter integer JUMP       = 7'b1101111;
+   parameter integer JUMP       = 7'b1101111;//jump and link(jal)
    parameter integer LOAD       = 7'b0000011;
    parameter integer STORE      = 7'b0100011;
 
    // RISC-V ALUOp[1:0] (see book Figure 4.12)
-   parameter [1:0] ADD_OPCODE     = 2'b00;
+      //ALU operation
+
+
+   parameter [1:0] ADD_OPCODE     = 2'b00;//branch and store 
    parameter [1:0] SUB_OPCODE     = 2'b01;
    parameter [1:0] R_TYPE_OPCODE  = 2'b10;
 
@@ -32,6 +35,7 @@ module control_unit(
    always@(*)begin
 
       case(opcode)
+
          ALU_R:begin
             alu_src   = 1'b0;
             mem_2_reg = 1'b0;
@@ -43,7 +47,61 @@ module control_unit(
             jump      = 1'b0;
          end
          
-         // Declare the control signals for each one of the instructions here...
+         ALU_I:begin//only for addi
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b1;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
+         
+         BRANCH_EQ:begin
+            alu_src   = 1'b0;
+            mem_2_reg = 1'b0;
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;
+            branch    = 1'b1;
+            alu_op    = SUB_OPCODE;
+            jump      = 1'b0;
+         end
+         
+         JUMP:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b0;//dc
+            reg_write = 1'b1;
+            mem_read  = 1'b0;
+            mem_write = 1'b0;//dc
+            branch    = 1'b0;//dont care
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b1;
+         end
+
+         LOAD:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b1;
+            reg_write = 1'b1;
+            mem_read  = 1'b1;
+            mem_write = 1'b0;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
+
+         STORE:begin
+            alu_src   = 1'b1;
+            mem_2_reg = 1'b0;//dc
+            reg_write = 1'b0;
+            mem_read  = 1'b0;
+            mem_write = 1'b1;
+            branch    = 1'b0;
+            alu_op    = ADD_OPCODE;
+            jump      = 1'b0;
+         end
+
 
          default:begin
             alu_src   = 1'b0;
